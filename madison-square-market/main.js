9061816c1eedcfd9f1e7444faff5b0094e151d17
gizmo <72733125+gospino123@users.noreply.github.com>
2021-11-14 14:44:04 -0500

    Fix resource references. Add jQuery menu fxn

--- madison-square-market/main.js
+++ madison-square-market/main.js
@@ -1,1 +1,25 @@
-// Empty
+$(document).ready(() => {
+    // Personal: Declare vars for jQuery objects
+    $cart = $('#cart');
+    $cartMenu = $('#cartMenu');
+    $account = $('#account');
+    $accountMenu = $('#accountMenu');
+    $help = $('#help');
+    $helpMenu = $('#helpMenu');
+  
+    // Add click event handlers for cart, account and help
+    $cart.on('click', () => {
+      $cartMenu.show();
+    });
+    $account.on('click', () => {
+      $accountMenu.show();
+    });
+    $help.on('click', () => {
+      $helpMenu.show();
+    });
+  
+    // Add mouseleave event handler to each '.dropdown-menu's
+    $('.dropdown-menu').on('mouseleave', () => {
+      $('.dropdown-menu').hide();
+    });
+  })
