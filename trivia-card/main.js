b759435972737c6916509895c8e751c8b9560b88
gizmo <72733125+gospino123@users.noreply.github.com>
2021-11-15 21:52:10 -0500

    Add card btn fxns. Add extra reset

--- trivia-card/main.js
+++ trivia-card/main.js
@@ -1,3 +1,44 @@
 $(document).ready(() =>{
+    // Hide smiley and show frown to replace Codecademy multiple use code
+    $noSmile = () => {
+        $('.smiley').hide();
+        $('.frown').show();
+    };
 
+    $('.hint-box').on('click', () => {
+        $('.hint').slideToggle('fast');
+    });
+
+    $('.wrong-answer-one').on('click', () => {
+        $('.wrong-text-one').fadeOut('fast');
+        $noSmile();
+    });
+
+    $('.wrong-answer-two').on('click', () => {
+        $('.wrong-text-two').fadeOut('fast');
+        $noSmile();
+    });
+    
+    $('.wrong-answer-three').on('click', () => {
+        $('.wrong-text-three').fadeOut('fast');
+        $noSmile();
+    });
+
+    $('.correct-answer').on('click', () => {
+        $('.frown').hide();
+        $('.wrong-text-one').fadeOut('fast');
+        $('.wrong-text-two').fadeOut('fast');
+        $('.wrong-text-three').fadeOut('fast');
+        $('.smiley').show();
+    });
+
+    // Add reset button as suggested
+    $('.reset-box').on('click', () => {
+        $('.smiley').hide();
+        $('.frown').hide();
+        $('.wrong-text-one').fadeIn('fast');
+        $('.wrong-text-two').fadeIn('fast');
+        $('.wrong-text-three').fadeIn('fast');
+        $('.hint').slideUp('fast');
+    });
 });
