4d2e037cbbf92d569534b8a8272150ecf8f3b0cf
gizmo <72733125+gospino123@users.noreply.github.com>
2021-11-20 15:10:34 -0500

    Add nav-menu functionality

--- feedster/main.js
+++ feedster/main.js
@@ -1,3 +1,9 @@
 $(document).ready(() => {
-
+    // Open/close nav-menu with mouse menu enter and nav-menu leave
+    $('.menu').on('mouseenter', () => {
+        $('.nav-menu').slideDown('fast');
+      });
+    $('.nav-menu').on('mouseleave', () => {
+    $('.nav-menu').slideUp('fast');
+    }); 
 }); 
