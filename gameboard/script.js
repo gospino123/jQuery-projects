90118f968a3755b5896491e12543c75b6c6de5bf
gizmo <72733125+gospino123@users.noreply.github.com>
2021-11-21 20:29:20 -0500

    Add button functions using DOM navigation

--- gameboard/script.js
+++ gameboard/script.js
@@ -1,5 +1,15 @@
 var main = function() {
+    $('.more-btn').on('click', event => {
+        $(event.currentTarget).siblings('.more-menu').toggle();
+    }); // Toggle more-menu individually for each card
 
+    $('.share').on('click', event => {
+        $(event.currentTarget).siblings('.share-menu').toggle();
+    }); // Toggle sub-menu share-menu individually for each card
+
+    $('.bell').on('click', event => {
+        $(event.currentTarget).find('.notification').toggleClass('active');
+    });    
 };
 
 $(document).ready(main);
